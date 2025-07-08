eaf25ae9e4666c3e1c15731e345e1f35cd0b4c24
aztris <aziztrisaputro03@gmail.com>
2025-07-08 21:36:19 +0700

    update verifikasi kehadiran

--- database/seeders/AcademicYearSeeder.php
+++ database/seeders/AcademicYearSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\AcademicYear;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class AcademicYearSeeder extends Seeder
 {
@@ -12,6 +13,18 @@ class AcademicYearSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        AcademicYear::create([
+            'name' => '2023/2024',
+            'start_date' => '2023-07-01',
+            'end_date' => '2024-06-30',
+            'is_active' => false,
+        ]);
+
+        AcademicYear::create([
+            'name' => '2024/2025',
+            'start_date' => '2024-07-01',
+            'end_date' => '2025-06-30',
+            'is_active' => true, // <-- INI YANG PENTING: Set satu tahun sebagai aktif
+        ]);
     }
 }
